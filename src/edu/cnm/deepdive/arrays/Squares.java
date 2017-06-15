ef28af9628b4dc59b5c2058f91d43917577febc7
kpescobar <kellyp.721@live.com>
2017-06-15 14:03:04 -0700

    Implemented expression for squares array.

--- src/edu/cnm/deepdive/arrays/Squares.java
+++ src/edu/cnm/deepdive/arrays/Squares.java
@@ -1,10 +1,15 @@
 package edu.cnm.deepdive.arrays;
 
+import java.util.Arrays;
+
 public class Squares {
 
   public static void main(String[] args) {
-
-
+    int[] squares = new int [50];
+    for (int i = 0; i < squares.length; i++) {
+      squares[i] = i * i;
+    }
+    System.out.println(Arrays.toString(squares));
   }
 
 }
